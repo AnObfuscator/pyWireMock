84449c89b2a9fbd99bb684a48eda058ff5680294
AnObfuscator <anobfuscator@gmail.com>
2017-11-15 23:08:27 -0600

    Add and update methods for __admin/mappings

--- pywiremock/client.py
+++ pywiremock/client.py
@@ -7,22 +7,47 @@ class WireMock:
         self._host = host if host else 'localhost'
         self._port = port
         self._url_prefix = url_prefix if url_prefix else ''
+        self._base_url = 'http://{}:{}/__admin'.format(self._host, self._port)
 
-    def save_mappings(self):
-        raise NotImplementedError
+    def list_all_stub_mappings(self, limit=None, offset=None):
+        url = '{}/mappings'.format(self._base_url)
+        response = requests.get(url) # TODO support limit & offset
+        return response.json()
+
+    def add_stub_mapping(self, stub_mapping):
+        url = '{}/mappings'.format(self._base_url)
+        response = requests.post(url, stub_mapping.to_json())
+        return response.json()
 
     def reset_mappings(self):
         """
         reset all mappings, including defaults
         """
-        url = 'http://{}:{}/__admin/reset'.format(self._host, self._port)
-        requests.post(url)
+        url = '{}/mappings'.format(self._base_url)
+        requests.delete(url)
 
     def reset_to_default_mappings(self):
         """
         reset mappings to defaults loaded from json
         """
-        url = 'http://{}:{}/__admin/mappings/reset'.format(self._host, self._port)
+        url = '{}/mappings/reset'.format(self._base_url)
+        requests.post(url)
+
+    def get_stub_mapping(self, mapping_id):
+        url = '{}/mappings/{}'.format(self._base_url, mapping_id)
+        response = requests.get(url)
+        return response.json()
+
+    def edit_stub_mapping(self, mapping_id, stub_mapping):
+        url = '{}/mappings/{}'.format(self._base_url, mapping_id)
+        response = requests.put(url, stub_mapping.to_json())
+
+    def remove_stub_mapping(self, mapping_id):
+        url = '{}/mappings/{}'.format(self._base_url, mapping_id)
+        requests.delete(url)
+
+    def save_mappings(self):
+        url = '{}/mappings/save'.format(self._base_url)
         requests.post(url)
 
     def reset_requests(self):
@@ -41,9 +66,6 @@ class WireMock:
         # print(stub_mapping.to_json())
         # print(result)
 
-    def list_all_stub_mappings(self):
-        raise NotImplementedError
-
     def set_global_fixed_delay(self, milliseconds):
         raise NotImplementedError
 
@@ -59,3 +81,7 @@ class WireMock:
         response_count = int(response_content['count'])
         if count != response_count:
             raise AssertionError('Assertion failed. Expected count: {} Actual count: {}'.format(count, response_count))
+
+    def shutdown(self):
+        url = '{}/shutdown'.format(self._base_url)
+        requests.post(url)
